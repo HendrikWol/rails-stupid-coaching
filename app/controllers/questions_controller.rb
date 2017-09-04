4157a813bdb04c7667c11b9b82821c2b65399102
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-04 12:28:38 +0200

    doen

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-
+    @question = params[:query]
+    if @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work right now!"
+      @answer = "Good job!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
